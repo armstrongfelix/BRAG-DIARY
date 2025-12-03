57aca2e287bf1508243a486131247837f006ca10
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2025-12-03 17:49:28 +0100

    commited

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+const person = {
+  name: "Bob",
+  age: 30,
+  sayHello: function () {
+    return "Hello, my name is " + this.name + " " + this.age;
+  },
+};
+
+console.log(person.sayHello());
+*/
